c7016d02279c17438f20853851c6563759ab37d9
dmiv <dmitriy40647274@gmail.com>
2025-03-17 18:25:09 +0300

    request

--- admin-panel/src/api/categories.ts
+++ admin-panel/src/api/categories.ts
@@ -1,6 +1,6 @@
 import { instance as axios } from "./axios";
 
 export const fetchCategories = async () => {
-    const response = await axios.get(`/categories`);
+    const response = await axios.get(`/categories/`);
     return response.data;
 };
